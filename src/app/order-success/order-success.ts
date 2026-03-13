f6ea0d79d481010983326078b32db00500ac1133
laknar <consultant-lakshmidurga.naramreddy@mcb.mu>
2026-03-13 12:02:16 +0530

    removed code

--- src/app/order-success/order-success.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-order-success',
-  imports: [],
-  templateUrl: './order-success.html',
-  styleUrl: './order-success.scss',
-})
-export class OrderSuccess {}
